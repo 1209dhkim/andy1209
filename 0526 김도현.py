097c4834586a45fd322c68d0dbf2547f5361dab7
1209dhkim <131439097+1209dhkim@users.noreply.github.com>
2023-05-26 16:46:38 +0900

    도농 방과후

---
+++ 0526 김도현.py
@@ -0,0 +1,26 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/05226 김도현(연산자).py
+19
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/05226 김도현(연산자).py
+19
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/05226 김도현(연산자).py
+a를 입력하세요:5
+b를 입력하세요:7
+연산자를 입력하세요:+
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python39/05226 김도현(연산자).py", line 6, in <module>
+    print(f"a+b={c}")
+NameError: name 'c' is not defined
+>>> 
+======= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/05226 김도현(연산자).py ======
+a를 입력하세요:6
+b를 입력하세요:8
+연산자를 입력하세요:+
+a+b=14
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/05226 김도현(연산자).py
+a를 입력하세요:
